e67d441d66638a74ada02aa40df1e0db4bc87cbf
Ruby Dang <93013511+Rubydang2292@users.noreply.github.com>
2023-02-23 22:29:19 -0600

    Update checkCurrentUser.js

--- back-end/middlewares/checkCurrentUser.js
+++ back-end/middlewares/checkCurrentUser.js
@@ -2,7 +2,6 @@ const jwt = require('jsonwebtoken');
 
 exports.checkCurrentUser = (req, res, next) => {
 
-    // Access Authorization from header
     const Authorization = req.header("Authorization");
 
     if (!Authorization) {
@@ -10,19 +9,16 @@ exports.checkCurrentUser = (req, res, next) => {
 
     } else {
 
-
         const token = Authorization.replace('Bearer ', "");
 
         // Verify token
         try {
 
-            
             const { userId } = jwt.verify(token, process.env.APP_SECRET);
             req.user = { userId }; 
             next();
 
         } catch (err) {
-            // Nếu không verify được token -> set user = null
             req.user = null;
             next();
         }
