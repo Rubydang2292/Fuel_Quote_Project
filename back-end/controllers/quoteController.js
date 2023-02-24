e3886e84350e6bf88587a435fab793c7ad2cddff
Ruby Dang <93013511+Rubydang2292@users.noreply.github.com>
2023-02-23 22:25:57 -0600

    Create quoteController.js

---
+++ back-end/controllers/quoteController.js
@@ -0,0 +1,52 @@
+const Quote = require('../models/Quote');
+
+// Get all quotes
+exports.getAllQuotes = async (req, res, next) =>{
+    try {
+        
+        const quotes = await Quote.find({}).populate('author');
+
+        res.status(200).json({
+            status: 'Success',
+            results: quotes.length,
+            data: { quotes }
+        });
+
+    } catch (error) {
+        res.json(error);
+    }
+};
+
+// Create one quote
+exports.createOneQuote = async (req, res, next) => {
+    try {
+        const {userId} = req.user;
+        const quote = await Quote.create( {...req.body, author: userId} );
+
+        // response back to client if successfully connect to DB
+        res.status(200).json({
+            status: 'Success',
+            data: { quote }
+        });
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+// Get all quotes
+exports.getUserQuotes = async (req, res, next) =>{
+    try {
+        const { userId } = req.params;
+        const quotes = await Quote.find({author: userId})
+
+        res.status(200).json({
+            status: 'Success',
+            results: quotes.length,
+            data: { quotes }
+        });
+
+    } catch (error) {
+        res.json(error);
+    }
+};
